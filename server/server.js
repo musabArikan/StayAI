8b51c9593ec02eff3d09ebc39512073729ca1033
Musab Arıkan <arikan950@hotmail.com>
2025-10-27 18:48:50 +0300

    feat: add MongoDB connection, Clerk webhook handling, and User model
    - Created a new database connection module (db.js) to connect to MongoDB.
    - Implemented Clerk webhook handling in a new controller (clerkWebHooks.js) to manage user events (created, updated, deleted).
    - Added a User model (User.js) to define the user schema for MongoDB.
    - Updated package.json to include necessary dependencies for Clerk and nodemon.
    - Modified server.js to establish database connection, set up middleware for Clerk, and define a basic API route for webhooks.

--- server/server.js
+++ server/server.js
@@ -1,7 +1,23 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
+import connectDB from "./configs/db.js";
+import { clerkMiddleware } from "@clerk/express";
+import clerkWebHooks from "./controllers/clerkWebHooks.js";
+connectDB();
 
 const app = express();
-
 app.use(cors());
+app.use(express.json());
+app.use(clerkMiddleware());
+
+app.use("/api/clerk", clerkWebHooks);
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
