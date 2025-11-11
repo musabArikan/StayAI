c91c698534e32fc402113b679c5aae73ead591c0
Musab Arıkan <arikan950@hotmail.com>
2025-11-11 16:09:29 +0300

    feat: integrate hotel registration and booking features
    - Added HotelReg component for hotel registration with form handling and API integration.
    - Updated App component to conditionally render HotelReg based on context state.
    - Implemented context provider (AppContext) to manage global state including user role and hotel registration visibility.
    - Enhanced Navbar to toggle between dashboard and hotel registration based on user role.
    - Created backend routes and controllers for hotel registration, room management, and booking functionalities.
    - Integrated Cloudinary for image uploads in room creation.
    - Established user and booking management routes with authentication middleware.
    - Added error handling and success notifications using react-hot-toast.

--- server/server.js
+++ server/server.js
@@ -4,7 +4,14 @@ import cors from "cors";
 import connectDB from "./configs/db.js";
 import { clerkMiddleware } from "@clerk/express";
 import clerkWebHooks from "./controllers/clerkWebHooks.js";
+import userRouter from "./routes/UserRoutes.js";
+import hotelRouter from "./routes/hotelRoutes.js";
+import connectCloudinary from "./configs/cloudinary.js";
+import roomRouter from "./routes/roomRoutes.js";
+import bookingRouter from "./routes/bookingRoutes.js";
+
 connectDB();
+connectCloudinary();
 
 const app = express();
 app.use(
@@ -21,7 +28,13 @@ app.use(express.json());
 app.use(clerkMiddleware());
 
 app.use("/api/clerk", clerkWebHooks);
-
+app.use("/api/user", userRouter);
+app.use("/api/hotels", hotelRouter);
+app.use("/api/rooms", roomRouter);
+app.use("/api/bookings", bookingRouter);
+app.get("/", (req, res) => {
+  res.send("StayAI Backend is running...");
+});
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
